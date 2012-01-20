0298544358f295ac5c21cc47e987cc4f7c5a2dc6
Borys Rashba <rashba@gmail.com>
2012-01-19 22:36:04 -0800

    Boryses To-Do List project has been just created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BorysesTodo::Application.config.secret_token = '28001a929b0d6d3c6705e7d59619db680069a8409cfb8b5e889c282745ada56d531b45634b2218424ab2589c920ad024c8e23ab39b83677ea18bccb7cf406726'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BorysesTodo::Application.config.session_store :cookie_store, key: '_boryses_todo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BorysesTodo::Application.config.session_store :active_record_store
